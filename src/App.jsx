b657e2a56bfa664dc13fdb0df59ea1dc50cabdf8
Oluwaseyi Feranmi Afolayan <afolayancseyi@gmail.com>
2023-10-17 01:41:01 +0100

    fully optimized portfolio website

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Experience />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
